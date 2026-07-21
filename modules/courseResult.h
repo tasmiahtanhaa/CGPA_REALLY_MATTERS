ac7825516bbdb8dc48ce5a711765a9c6ed40d3b6
Tasmiah Tanha <tasmiahtanhaa@gmail.com>
2026-07-21 14:52:05 +0600

    Declared semester course helpers

--- modules/courseResult.h
+++ modules/courseResult.h
@@ -10,6 +10,9 @@ typedef struct CourseResult
 } CourseResult;
 
 CourseResult createCourseResult(Course *course, double marks);
+void sortCourseResultsBySemester(CourseResult results[], int n_results);
+void filterCourseResultsBySemester(CourseResult results[], int n_results, int semester, CourseResult filtered[]);
+int countCourseResultsBeforeNull(CourseResult results[], int n_results);
 void viewCourseResult(CourseResult result);
 
 #endif

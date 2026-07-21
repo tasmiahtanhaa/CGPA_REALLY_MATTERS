82b4db804b7d20af74d641f092b3f40998e1325a
Tasmiah Tanha <tasmiahtanhaa@gmail.com>
2026-07-21 14:53:28 +0600

    Displayed semester wise GPA in main

--- main.c
+++ main.c
@@ -9,9 +9,10 @@ int main()
     CourseResult results[1000];
     int n_courses = 0;
 
-    courses[n_courses++] = createCourse("CSE 4107", "Structured Programming I", 3.0);
-    courses[n_courses++] = createCourse("CSE 4108", "Structured Programming I Lab", 1.5);
-    courses[n_courses++] = createCourse("CSE 4203", "Discrete Mathematics", 3.0);
+    courses[n_courses++] = createCourse("CSE 4107", "Structured Programming I", 3.0, 1);
+    courses[n_courses++] = createCourse("CSE 4108", "Structured Programming I Lab", 1.5, 1);
+    courses[n_courses++] = createCourse("CSE 4203", "Discrete Mathematics", 3.0, 2);
+    courses[n_courses++] = createCourse("CSE 4205", "Digital Logic Design", 3.0, 2);
 
     for (int i = 0; i < n_courses; i++)
     {
@@ -21,12 +22,23 @@ int main()
         results[i] = createCourseResult(&courses[i], marks);
     }
 
-    printf("\nResults\n");
-    for (int i = 0; i < n_courses; i++)
+    sortCourseResultsBySemester(results, n_courses);
+
+    for (int semester = 1; semester <= 8; semester++)
     {
-        viewCourseResult(results[i]);
-        printf("Grade: %s\n", getLetterGrade(results[i]));
+        CourseResult semesterResults[100];
+        int n_semester_results;
+
+        filterCourseResultsBySemester(results, n_courses, semester, semesterResults);
+        n_semester_results = countCourseResultsBeforeNull(semesterResults, 100);
+
+        if (n_semester_results > 0)
+        {
+            printf("Semester %d GPA: %.2f\n",
+                   semester, calculateGPA(semesterResults, n_semester_results));
+        }
     }
+
     printf("CGPA: %.2f\n", calculateGPA(results, n_courses));
 
     return 0;
